c2c5d96e3815e9a50249de9585d81a8183fd0bbe
jesus091101 <elberjesusquirozc09@gmail.com>
2024-03-25 19:31:39 -0500

    v1

--- src/app/components/private/private.module.ts
+++ src/app/components/private/private.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PrivateRoutingModule } from './private-routing.module';
+import { UsuariosComponent } from './Mantenimientos/usuarios/usuarios.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UsuariosComponent
+  ],
   imports: [
     CommonModule,
     PrivateRoutingModule
