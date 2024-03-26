e0ce48e85b4e36276654e4e8d982ee04fcbef150
FASDA0311 <fserrepe@unprg.edu.pe>
2024-03-25 19:36:36 -0500

    enrutado provesional

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full',
+},
+{
+    path : '',
+    children: [
+        {
+            path: '',
+            loadChildren: () => import('./components/public/public.module').then(m => m.PublicModule),
+        },
+        {
+            path: '',
+            loadChildren: () => import('./components/private/private.module').then(m => m.PrivateModule),
+        }
+    ]
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/private/private-routing.module.ts
+++ src/app/components/private/private-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsuariosComponent } from './Mantenimientos/usuarios/usuarios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path : "1",
+    component : UsuariosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
