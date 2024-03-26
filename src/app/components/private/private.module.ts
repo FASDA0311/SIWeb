824eb808e5e4a48dcc3ab152dae4790fbfc4ecbf
jesus091101 <elberjesusquirozc09@gmail.com>
2024-03-25 21:20:54 -0500

    a

--- src/app/components/private/private.module.ts
+++ src/app/components/private/private.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { PrivateRoutingModule } from './private-routing.module';
 import { UsuariosComponent } from './Mantenimientos/usuarios/usuarios.component';
+import { LayoutComponent } from './layout/layout.component';
 
 
 @NgModule({
   declarations: [
-    UsuariosComponent
+    UsuariosComponent,
+    LayoutComponent
   ],
   imports: [
     CommonModule,
